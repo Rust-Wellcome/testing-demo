7ce83bfa4731cc945560e8ff3950fed88d5add20
Will Burden <wb6@sanger.ac.uk>
2023-03-15 20:04:20 +0000

    Add unit tests

--- src/factorial.rs
+++ src/factorial.rs
@@ -4,3 +4,27 @@
 pub fn factorial(x: u128) -> Option<u128> {
     (2..=x).try_fold(1, u128::checked_mul)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial;
+
+    #[test]
+    fn test_factorial() {
+        assert_eq!(factorial(5), Some(120));
+        assert_eq!(factorial(9), Some(362880));
+        assert_eq!(factorial(12), Some(479001600));
+    }
+    
+    #[test]
+    fn test_factorial_low_boundary() {
+        assert_eq!(factorial(0), Some(1));
+        assert_eq!(factorial(1), Some(1));
+        assert_eq!(factorial(2), Some(2));
+    }
+
+    #[test]
+    fn test_factorial_overflow() {
+        assert_eq!(factorial(50), None);
+    }
+}
