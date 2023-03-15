3301847214effef54ddbeeaee12bf4100dc62729
Will Burden <wb6@sanger.ac.uk>
2023-03-15 20:05:05 +0000

    Add doctests

--- src/factorial.rs
+++ src/factorial.rs
@@ -1,5 +1,21 @@
 /// Computes the [factorial] of a `u128`.
 /// 
+/// # Examples
+/// Basic usage:
+/// ```
+/// # use testing_demo::factorial;
+/// assert_eq!(factorial(0), Some(1));
+/// assert_eq!(factorial(1), Some(1));
+/// assert_eq!(factorial(3), Some(6));
+/// assert_eq!(factorial(5), Some(120));
+/// ```
+/// 
+/// `None` is returned if the result would exceed [`u128::MAX`]:
+/// ```
+/// # use testing_demo::factorial;
+/// assert_eq!(factorial(50), None);
+/// ```
+/// 
 /// [factorial]: https://en.wikipedia.org/wiki/Factorial
 pub fn factorial(x: u128) -> Option<u128> {
     (2..=x).try_fold(1, u128::checked_mul)
